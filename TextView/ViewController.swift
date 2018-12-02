effe38bf55b56d137c3d41499a6de7c019c00303
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-02 11:50:21 +0300

    init commit

--- TextView/ViewController.swift
+++ TextView/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//      1. Set delegate
+        textView.delegate = self
+        
+        textView.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 17)
+        textView.backgroundColor = self.view.backgroundColor
+        
+        textView.layer.cornerRadius = 10
     }
 
+//    2. Set Hiding keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+//        Hides keyboard for any object
+//        self.view.endEditing(true)
+        
+//        Hides keyboard for particular object
+        textView.resignFirstResponder()
+        
+    }
 
 }
 
+extension ViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.backgroundColor = .white
+        textView.textColor = .gray
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.backgroundColor = self.view.backgroundColor
+        textView.textColor = .black
+    }
+}
